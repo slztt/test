af116992224a46451042193be2f549abf4361565
slztt <lclarke234567@gmail.com>
2025-05-05 00:17:48 +0100

    Delete my-nextjs-app directory

--- my-nextjs-app/pages/index.js
+++
@@ -1,7 +0,0 @@
-export default function Home() {
-  return (
-    <div className="flex items-center justify-center min-h-screen bg-gray-100">
-      <h1 className="text-4xl font-bold text-gray-800">Hello, world!</h1>
-    </div>
-  );
-}
